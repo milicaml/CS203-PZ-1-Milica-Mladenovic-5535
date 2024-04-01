bd26f6fe6b35324a99687326898ee852ad498a58
milicaml <152500785+milicaml@users.noreply.github.com>
2024-04-01 14:25:44 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.cs203pz1milicamladenovic5535 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.cs203pz1milicamladenovic5535 to javafx.fxml;
+    exports com.cs203pz1milicamladenovic5535;
+}
